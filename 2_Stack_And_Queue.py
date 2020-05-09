0c61b41041caf6a36197aaf7c418f527a0d1bc82
aryanbuzz9 <49128207+aryanbuzz9@users.noreply.github.com>
2020-05-09 13:29:59 +0530

    Stack and Queue

---
+++ 2_Stack_And_Queue.py
@@ -0,0 +1,92 @@
+#Stack Implementation Using Array(List) In Python
+class Stack:
+  def __init__(self,n):
+    self.arr=[None]*3
+    self.top=-1
+    self.size=n
+  def push(self,data):
+    if(self.top<self.size):
+      self.top=self.top+1
+      self.arr[self.top]=data
+    else:
+      print("Overflow")
+      exit(1)
+  def pop(self):
+    if(self.top>=0):
+
+      data=self.arr[self.top]
+      self.top=self.top-1
+      return data
+    else:
+      print("underflow")
+      exit(1)
+obj=Stack(3)
+obj.push(7)
+obj.push(98)
+obj.push(19)
+#obj.push(67)
+obj.pop()
+obj.pop()
+obj.pop()
+obj.pop()
+print(obj.arr)
+
+#Queue implementation Using Array(List) In Python
+class Queue: 
+
+	def __init__(self, capacity): 
+		self.front = self.size = 0
+		self.rear = capacity -1
+		self.arr = [None]*capacity 
+		self.capacity = capacity 
+
+	def isFull(self): 
+		return self.size == self.capacity 
+
+	def isEmpty(self): 
+		return self.size == 0
+
+	def EnQueue(self, item): 
+		if self.isFull(): 
+			print("Full") 
+			return
+		self.rear = (self.rear + 1) % (self.capacity) 
+		self.arr[self.rear] = item 
+		self.size = self.size + 1
+		print(item) 
+
+	def DeQueue(self): 
+		if self.isEmpty(): 
+			print("Empty") 
+			return
+		
+		print(self.arr[self.front]) 
+		self.front = (self.front + 1) % (self.capacity) 
+		self.size = self.size -1
+
+	def que_front(self): 
+		if self.isEmpty(): 
+			print("Queue is empty") 
+
+		print("Front item is", self.arr[self.front]) 
+
+	def que_rear(self): 
+		if self.isEmpty(): 
+			print("Queue is empty") 
+		print("Rear item is", self.arr[self.rear]) 
+ 
+
+queue = Queue(10) 
+print("Added items")
+queue.EnQueue(10) 
+queue.EnQueue(20) 
+queue.EnQueue(30) 
+queue.EnQueue(40) 
+print("Deleted items")
+queue.DeQueue()
+
+queue.que_front()
+
+queue.que_rear() 
+
+
