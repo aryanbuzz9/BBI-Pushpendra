6a703c843a67de647b3e698db88daf90a39e92eb
aryanbuzz9 <49128207+aryanbuzz9@users.noreply.github.com>
2020-05-11 11:42:11 +0530

    Pancakes Problem

---
+++ 6_Pancakes.py
@@ -0,0 +1,15 @@
+s=input()
+for i in s:
+    a.append(i)
+
+step=0
+for i in range(len(a)-1):
+    if(a[i]=='-' and a[i+1]=='+'):
+        a[i]='+'
+        a[i+1]='+'
+        step=step+1
+    elif(a[i]=='+' and a[i+1]=='-'):
+        a[i]='+'
+        a[i+1]='-'
+        step=step+1
+print(step)
