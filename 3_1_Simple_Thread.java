036db0fe28499b1be604f542eff016cb6492ab9d
aryanbuzz9 <49128207+aryanbuzz9@users.noreply.github.com>
2020-05-11 11:46:01 +0530

    Delete 3_1_Simple_Thread.java

--- 3_1_Simple_Thread.java
+++
@@ -1,9 +0,0 @@
-class Bbi_Training extends Thread{  
-public void run(){  
-System.out.println("Simple Thread is running...");  
-}  
-public static void main(String args[]){  
-Bbi_Training obj=new Bbi_Training();  
-obj.start();  
- }  
-}  
